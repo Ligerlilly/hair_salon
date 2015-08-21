11e2df6109b61db39db7b8ad81ade206a8fdce70
Jason <quizathaderat@yahoo.com>
2015-08-21 10:34:41 -0700

    Add stylist_id to initialize method and add find method to Client

--- lib/client.rb
+++ lib/client.rb
@@ -1,8 +1,21 @@
 class Client
-  attr_reader :id, :name
+  attr_reader :id, :name, :stylist_id
 
   def initialize(attributes)
     @id = attributes[:id].to_i
     @name = attributes[:name]
+    @stylist_id = attributes[:stylist_id].to_i
+  end
+
+  def self.all
+    clients = []
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    returned_clients.each do |client|
+      name = client['name']
+      id = client['id'].to_i
+      stylist_id = client['stylist_id'].to_id
+      clients.push(Client.new({ id: id, name: name, stylist_id: stylist_id }))
+    end
+    clients
   end
 end
--- spec/clients_spec.rb
+++ spec/clients_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Client do
   before do
-    @client = Client.new({ id: nil, name: 'Lisa' })
+    @client = Client.new({ id: nil, name: 'Lisa', stylist_id: nil })
   end
   describe '#id' do
     it 'should be 0 at first' do
@@ -10,9 +10,21 @@ describe Client do
     end
   end
 
-  describe '#nane' do
+  describe '#name' do
     it 'should be 0 at first' do
       expect(@client.name).to eq 'Lisa'
+    end
+  end
+
+  describe '.all' do
+    it 'should return an empty array at first' do
+      expect(Client.all).to eq []
+    end
+  end
+
+  describe '#stylist_id' do
+    it "should return id of client's stylist, should be zero at first" do
+      expect(@client.stylist_id).to eq 0
     end
   end
 end
